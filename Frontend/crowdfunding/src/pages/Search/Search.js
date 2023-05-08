c8126ef7115a57a7ab7a0c4c762b2b8403d384cf
HeaganLee <ekzmfkdl12@gmail.com>
2023-05-08 17:13:48 +0900

    2023-05-08 css 분리

--- Frontend/crowdfunding/src/pages/Search/Search.js
+++ Frontend/crowdfunding/src/pages/Search/Search.js
@@ -3,285 +3,16 @@ import { css } from '@emotion/react';
 import React, { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import { HiHome } from 'react-icons/hi';
+import * as S from './style'
 
-const searchMainContainer = css`
-    width: 900px;
-    height: 2000px;
-    margin: auto;
-    margin-top: 10px;
-`;
-
-const searchBox = css`
-    padding: 0px;
-    margin: 50px auto;
-    width: 760px;
-    height: 50px;
-    border: 2px solid green;
-`
-
-const searchInput = css`
-    padding-left: 2px;
-    border: none;
-    width: 93%;
-    height: 80%;
-
-    font-size: 20px;
-
-    &:focus {
-    outline:none;   
-    }
-`;
-
-const searchButton = css`
-    border: none;
-    width: 7%;
-    height: 100%;
-
-    font-size: 20px;
-
-    cursor: pointer;
-`
-
-const searchResultBox = css`
-    margin: auto;
-    margin-top: 100px;
-
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    width: 800px;
-`;
-
-const searchResultCategory = css`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    
-    width: 100%;
-    height: 50px;
-
-    border-bottom: 1px solid black;
-`;
-
-const falseCategoryButton = css`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    
-    padding-bottom: 10px;
-    margin: 0px 50px;
-    width: 20%;
-    height: 100%;
-
-    border: none;
-    
-    background-color: white;
-    font-size: 30px;
-
-    cursor: pointer;
-`;
-
-const trueCategoryButton = css`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    
-    padding-bottom: 10px;
-    margin: 0px 50px;
-    width: 20%;
-    height: 100%;
-
-    border: none;
-    
-    background-color: #003d1e;
-    font-size: 30px;
-    color: #00Ab33;
-
-    cursor: pointer;
-`;
-
-const searchResultContainer = css`
-    margin-top: 30px;
-    width: 100%;
-`
-
-const searchResultTop = css`
-    display: flex;
-    justify-content: space-between;
-`;
-
-const searchResultLeft = css`
-    margin-top: 7px;
-
-`;
-
-const searchResultRight = css`
-    display: flex;
-    justify-content: space-between;
-    width: 250px;
-    height: 50px;
-
-`;
-
-const newestButton = css`
-    margin-top: 10px;
-    
-    width: 30%;
-    height: 20px;
-
-    border: none;
-    background-color: white;
-`;
-
-const amountButton = css`
-    margin-top: 10px;
-    margin-right: 25px;
-    width: 30%;
-    height: 20px;
-
-    border: none;
-    background-color: white;
-`;
-
-const searchSortContainer = css`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    width: 40%;
-    height: 200px;
-`;
-
-const searchSortButtonContainer = css`
-    display: flex;
-    justify-content: center;
-    
-    width: 100%;
-    height: 20%;
-
-    border: 1px solid black;
-    
-    background-color: white;
-    
-    cursor: pointer;
-`;
-
-const searchSortButton = css`
-    width: 80%;
-    height: 100%;
-
-    border: none;
-    font-size: 15px;
-    background-color: white;
-`;
-const direction = css`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 20%;
-    height: 100%;
-
-    font-size: 20px;
-    border: none;
-`;
-
-const sortCategoryList = css`
-    width: 100%;
-    
-    border: 1px solid black;
-    border-top: none;
-    cursor: pointer;
-`;
-
-const sortCategory = css`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    margin-top: 15px;
-    margin-bottom: 10px;
-`;
-
-const searchResultPanel = css`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    padding: 20px 0px;
-    width: 100%;
-    height: 130px;
-
-    border-bottom: 1px solid #dbdbdb;
-`;
-
-const panelImgContainer = css`
-    width: 15%;
-`;
-
-const panelImg = css`
-    width: 100%;
-`
-
-const panelInfo = css`
-    display: flex;
-    flex-direction: column;
-
-    padding-left: 20px;
-    width: 65%;
-`;
-
-const panelTitle = css`
-    display: flex;
-`;
-
-const progress = css`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    padding: 5px;
-    width: 50px;
-    background-color: green;
-    color: white;
-
-    font-size: 12px;
-`;
-
-const panelTitleText = css`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    padding: 5px;
-    font-size: 15px;
-`;
-
-const centerName = css`
-    display: flex;
-    
-    padding-top: 10px;
-    font-size: 14px;
-`;
-
-const panelTotalAmount = css`
-    display: flex;
-   
-    width:20%;
-
-    font-size: 20px;
-`;
-
-const Amount = css`
-    margin: 0px 10px;
-    color: green;
-`;
 
 const Search = () => {
     const [giveflage, setGiveflage] = useState(false);
     const [fundflage, setFundflage] = useState(false);
 
     const [sortHidenFlage, setSortHidenFlage] = useState(false);
+
+    const [sortTemaWord, setSortTemaWord] = useState("최신순")
 
     const [sortWord, setSortWord] = useState("전체");
 
@@ -299,6 +30,10 @@ const Search = () => {
         }
     }
 
+    const sortTemaWordChange = (e) => {
+        setSortTemaWord(e.target.textContent);
+    }
+
     const sortHiden = () => {
         if(sortHidenFlage) {
             setSortHidenFlage(false)
@@ -312,118 +47,118 @@ const Search = () => {
     }
 
     return (
-        <div css={searchMainContainer}>
-            <div css={searchBox}>
-                <input type="text" css={searchInput} placeholder='검색어를 입력해 주세요'/>
-                <button css={searchButton}><FiSearch/></button>
+        <div css={S.searchMainContainer}>
+            <div css={S.searchBox}>
+                <input type="text" css={S.searchInput} placeholder='검색어를 입력해 주세요'/>
+                <button css={S.searchButton}><FiSearch/></button>
             </div>
-            <div css={searchResultBox}>
-                <div css={searchResultCategory}>
-                    <button onClick={giveSateChange} css={giveflage ? trueCategoryButton : falseCategoryButton}>기부</button>
-                    <button onClick={fundSateChange} css={fundflage ? trueCategoryButton : falseCategoryButton}>펀딩</button>
+            <div css={S.searchResultBox}>
+                <div css={S.searchResultCategory}>
+                    <button onClick={giveSateChange} css={giveflage ? S.trueCategoryButton : S.falseCategoryButton}>기부</button>
+                    <button onClick={fundSateChange} css={fundflage ? S.trueCategoryButton : S.falseCategoryButton}>펀딩</button>
                 </div>
-                <div css={searchResultContainer}>
-                    <div css={searchResultTop}>
-                        <div css={searchResultLeft}>검색결과 " " 건</div>
-                        <div css={searchResultRight}>
-                           <button css={newestButton}>최신순</button>
-                           <button css={amountButton}>금액순</button>
-                           <div css={searchSortContainer}>
-                                <div css={searchSortButtonContainer} onClick={sortHiden}>
-                                    <button css={searchSortButton} >{sortWord}</button>
-                                    <div css={direction}>{sortHidenFlage ? "∧" : "∨"} </div>
+                <div css={S.searchResultContainer}>
+                    <div css={S.searchResultTop}>
+                        <div css={S.searchResultLeft}>검색결과 " " 건</div>
+                        <div css={S.searchResultRight}>
+                           <button onClick={sortTemaWordChange} css={S.newestButton}>최신순</button>
+                           <button onClick={sortTemaWordChange} css={S.amountButton}>금액순</button>
+                           <div css={S.searchSortContainer}>
+                                <div css={S.searchSortButtonContainer} onClick={sortHiden}>
+                                    <button css={S.searchSortButton} >{sortWord}</button>
+                                    <div css={S.direction}>{sortHidenFlage ? "∧" : "∨"} </div>
                                 </div>
-                                {sortHidenFlage ? (<ul css={sortCategoryList}>
-                                    <li css={sortCategory} onClick={sortWordChange}>전체</li>
-                                    <li css={sortCategory} onClick={sortWordChange}>진행중</li>
-                                    <li css={sortCategory} onClick={sortWordChange}>종료</li>
+                                {sortHidenFlage ? (<ul css={S.sortCategoryList}>
+                                    <li css={S.sortCategory} onClick={sortWordChange}>전체</li>
+                                    <li css={S.sortCategory} onClick={sortWordChange}>진행중</li>
+                                    <li css={S.sortCategory} onClick={sortWordChange}>종료</li>
                                     </ul>) : ""}
                            </div>
                         </div>
                     </div>
-                    <div css={searchResultPanel}>
-                        <div css={panelImgContainer}>
-                            <img css={panelImg} src="https://happybean-phinf.pstatic.net/20230428_23/1682638883837QfVjU_JPEG/KakaoTalk_20230427_201547249_01jpg?type=w720"
+                    <div css={S.searchResultPanel}>
+                        <div css={S.panelImgContainer}>
+                            <img css={S.panelImg} src="https://happybean-phinf.pstatic.net/20230428_23/1682638883837QfVjU_JPEG/KakaoTalk_20230427_201547249_01jpg?type=w720"
                             alt='https://happybean-phinf.pstatic.net/20230428_23/1682638883837QfVjU_JPEG/KakaoTalk_20230427_201547249_01jpg?type=w720'/>
                         </div>
-                        <div css={panelInfo}>
-                            <div css={panelTitle}>
-                                <div css={progress}>진행중</div>
-                                <div css={panelTitleText}> 행복한 어린이 날</div>    
+                        <div css={S.panelInfo}>
+                            <div css={S.panelTitle}>
+                                <div css={S.progress}>진행중</div>
+                                <div css={S.panelTitleText}> 행복한 어린이 날</div>    
                             </div>
-                            <div css={centerName}> <HiHome/> 모두의 마블</div>
+                            <div css={S.centerName}> <HiHome/> 모두의 마블</div>
                         </div>
-                        <div css={panelTotalAmount}>총 <div css={Amount}>5,555,000</div> 원</div>
+                        <div css={S.panelTotalAmount}>총 <div css={S.Amount}>5,555,000</div> 원</div>
                     </div>
-                     <div css={searchResultPanel}>
-                        <div css={panelImgContainer}>
-                            <img css={panelImg} src="https://happybean-phinf.pstatic.net/20230428_23/1682638883837QfVjU_JPEG/KakaoTalk_20230427_201547249_01jpg?type=w720"
+                     <div css={S.searchResultPanel}>
+                        <div css={S.panelImgContainer}>
+                            <img css={S.panelImg} src="https://happybean-phinf.pstatic.net/20230428_23/1682638883837QfVjU_JPEG/KakaoTalk_20230427_201547249_01jpg?type=w720"
                             alt='https://happybean-phinf.pstatic.net/20230428_23/1682638883837QfVjU_JPEG/KakaoTalk_20230427_201547249_01jpg?type=w720'/>
                         </div>
-                        <div css={panelInfo}>
-                            <div css={panelTitle}>
-                                <div css={progress}>진행중</div>
-                                <div css={panelTitleText}> 행복한 어린이 날</div>    
+                        <div css={S.panelInfo}>
+                            <div css={S.panelTitle}>
+                                <div css={S.progress}>진행중</div>
+                                <div css={S.panelTitleText}> 행복한 어린이 날</div>    
                             </div>
-                            <div css={centerName}> <HiHome/> 모두의 마블</div>
+                            <div css={S.centerName}> <HiHome/> 모두의 마블</div>
                         </div>
-                        <div css={panelTotalAmount}>총 <div css={Amount}>5,555,000</div> 원</div>
+                        <div css={S.panelTotalAmount}>총 <div css={S.Amount}>5,555,000</div> 원</div>
                     </div>
-                     <div css={searchResultPanel}>
-                        <div css={panelImgContainer}>
-                            <img css={panelImg} src="https://happybean-phinf.pstatic.net/20230428_23/1682638883837QfVjU_JPEG/KakaoTalk_20230427_201547249_01jpg?type=w720"
+                     <div css={S.searchResultPanel}>
+                        <div css={S.panelImgContainer}>
+                            <img css={S.panelImg} src="https://happybean-phinf.pstatic.net/20230428_23/1682638883837QfVjU_JPEG/KakaoTalk_20230427_201547249_01jpg?type=w720"
                             alt='https://happybean-phinf.pstatic.net/20230428_23/1682638883837QfVjU_JPEG/KakaoTalk_20230427_201547249_01jpg?type=w720'/>
                         </div>
-                        <div css={panelInfo}>
-                            <div css={panelTitle}>
-                                <div css={progress}>진행중</div>
-                                <div css={panelTitleText}> 행복한 어린이 날</div>    
+                        <div css={S.panelInfo}>
+                            <div css={S.panelTitle}>
+                                <div css={S.progress}>진행중</div>
+                                <div css={S.panelTitleText}> 행복한 어린이 날</div>    
                             </div>
-                            <div css={centerName}> <HiHome/> 모두의 마블</div>
+                            <div css={S.centerName}> <HiHome/> 모두의 마블</div>
                         </div>
-                        <div css={panelTotalAmount}>총 <div css={Amount}>5,555,000</div> 원</div>
+                        <div css={S.panelTotalAmount}>총 <div css={S.Amount}>5,555,000</div> 원</div>
                     </div>
-                     <div css={searchResultPanel}>
-                        <div css={panelImgContainer}>
-                            <img css={panelImg} src="https://happybean-phinf.pstatic.net/20230428_23/1682638883837QfVjU_JPEG/KakaoTalk_20230427_201547249_01jpg?type=w720"
+                     <div css={S.searchResultPanel}>
+                        <div css={S.panelImgContainer}>
+                            <img css={S.panelImg} src="https://happybean-phinf.pstatic.net/20230428_23/1682638883837QfVjU_JPEG/KakaoTalk_20230427_201547249_01jpg?type=w720"
                             alt='https://happybean-phinf.pstatic.net/20230428_23/1682638883837QfVjU_JPEG/KakaoTalk_20230427_201547249_01jpg?type=w720'/>
                         </div>
-                        <div css={panelInfo}>
-                            <div css={panelTitle}>
-                                <div css={progress}>진행중</div>
-                                <div css={panelTitleText}> 행복한 어린이 날</div>    
+                        <div css={S.panelInfo}>
+                            <div css={S.panelTitle}>
+                                <div css={S.progress}>진행중</div>
+                                <div css={S.panelTitleText}> 행복한 어린이 날</div>    
                             </div>
-                            <div css={centerName}> <HiHome/> 모두의 마블</div>
+                            <div css={S.centerName}> <HiHome/> 모두의 마블</div>
                         </div>
-                        <div css={panelTotalAmount}>총 <div css={Amount}>5,555,000</div> 원</div>
+                        <div css={S.panelTotalAmount}>총 <div css={S.Amount}>5,555,000</div> 원</div>
                     </div>
-                     <div css={searchResultPanel}>
-                        <div css={panelImgContainer}>
-                            <img css={panelImg} src="https://happybean-phinf.pstatic.net/20230428_23/1682638883837QfVjU_JPEG/KakaoTalk_20230427_201547249_01jpg?type=w720"
+                     <div css={S.searchResultPanel}>
+                        <div css={S.panelImgContainer}>
+                            <img css={S.panelImg} src="https://happybean-phinf.pstatic.net/20230428_23/1682638883837QfVjU_JPEG/KakaoTalk_20230427_201547249_01jpg?type=w720"
                             alt='https://happybean-phinf.pstatic.net/20230428_23/1682638883837QfVjU_JPEG/KakaoTalk_20230427_201547249_01jpg?type=w720'/>
                         </div>
-                        <div css={panelInfo}>
-                            <div css={panelTitle}>
-                                <div css={progress}>진행중</div>
-                                <div css={panelTitleText}> 행복한 어린이 날</div>    
+                        <div css={S.panelInfo}>
+                            <div css={S.panelTitle}>
+                                <div css={S.progress}>진행중</div>
+                                <div css={S.panelTitleText}> 행복한 어린이 날</div>    
                             </div>
-                            <div css={centerName}> <HiHome/> 모두의 마블</div>
+                            <div css={S.centerName}> <HiHome/> 모두의 마블</div>
                         </div>
-                        <div css={panelTotalAmount}>총 <div css={Amount}>5,555,000</div> 원</div>
+                        <div css={S.panelTotalAmount}>총 <div css={S.Amount}>5,555,000</div> 원</div>
                     </div>
-                     <div css={searchResultPanel}>
-                        <div css={panelImgContainer}>
-                            <img css={panelImg} src="https://happybean-phinf.pstatic.net/20230428_23/1682638883837QfVjU_JPEG/KakaoTalk_20230427_201547249_01jpg?type=w720"
+                     <div css={S.searchResultPanel}>
+                        <div css={S.panelImgContainer}>
+                            <img css={S.panelImg} src="https://happybean-phinf.pstatic.net/20230428_23/1682638883837QfVjU_JPEG/KakaoTalk_20230427_201547249_01jpg?type=w720"
                             alt='https://happybean-phinf.pstatic.net/20230428_23/1682638883837QfVjU_JPEG/KakaoTalk_20230427_201547249_01jpg?type=w720'/>
                         </div>
-                        <div css={panelInfo}>
-                            <div css={panelTitle}>
-                                <div css={progress}>진행중</div>
-                                <div css={panelTitleText}> 행복한 어린이 날</div>    
+                        <div css={S.panelInfo}>
+                            <div css={S.panelTitle}>
+                                <div css={S.progress}>진행중</div>
+                                <div css={S.panelTitleText}> 행복한 어린이 날</div>    
                             </div>
-                            <div css={centerName}> <HiHome/> 모두의 마블</div>
+                            <div css={S.centerName}> <HiHome/> 모두의 마블</div>
                         </div>
-                        <div css={panelTotalAmount}>총 <div css={Amount}>5,555,000</div> 원</div>
+                        <div css={S.panelTotalAmount}>총 <div css={S.Amount}>5,555,000</div> 원</div>
                     </div>
                 </div>
             </div>
---
+++ Frontend/crowdfunding/src/pages/Search/style.js
@@ -0,0 +1,282 @@
+/** @jsxImportSource @emotion/react */
+import {css} from "@emotion/react";
+
+export const searchMainContainer = css`
+    width: 900px;
+    height: 2000px;
+    margin: auto;
+    margin-top: 10px;
+`;
+
+export const searchBox = css`
+    padding: 0px;
+    margin: 50px auto;
+    width: 760px;
+    height: 50px;
+    border: 2px solid green;
+`
+
+export const searchInput = css`
+    padding-left: 2px;
+    border: none;
+    width: 93%;
+    height: 80%;
+
+    font-size: 20px;
+
+    &:focus {
+    outline:none;   
+    }
+`;
+
+export const searchButton = css`
+    border: none;
+    width: 7%;
+    height: 100%;
+
+    font-size: 20px;
+
+    cursor: pointer;
+`
+
+export const searchResultBox = css`
+    margin: auto;
+    margin-top: 100px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    width: 800px;
+`;
+
+export const searchResultCategory = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    width: 100%;
+    height: 50px;
+
+    border-bottom: 1px solid black;
+`;
+
+export const falseCategoryButton = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    padding-bottom: 10px;
+    margin: 0px 50px;
+    width: 20%;
+    height: 100%;
+
+    border: none;
+    
+    background-color: white;
+    font-size: 30px;
+
+    cursor: pointer;
+`;
+
+export const trueCategoryButton = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    padding-bottom: 10px;
+    margin: 0px 50px;
+    width: 20%;
+    height: 100%;
+
+    border: none;
+    
+    background-color: #003d1e;
+    font-size: 30px;
+    color: #00Ab33;
+
+    cursor: pointer;
+`;
+
+export const searchResultContainer = css`
+    margin-top: 30px;
+    width: 100%;
+`
+
+export const searchResultTop = css`
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const searchResultLeft = css`
+    margin-top: 7px;
+
+`;
+
+export const searchResultRight = css`
+    display: flex;
+    justify-content: space-between;
+    width: 250px;
+    height: 50px;
+
+`;
+
+export const newestButton = css`
+    margin-top: 10px;
+    
+    width: 30%;
+    height: 20px;
+
+    border: none;
+    background-color: white;
+`;
+
+export const amountButton = css`
+    margin-top: 10px;
+    margin-right: 25px;
+    width: 30%;
+    height: 20px;
+
+    border: none;
+    background-color: white;
+`;
+
+export const searchSortContainer = css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    width: 40%;
+    height: 200px;
+`;
+
+export const searchSortButtonContainer = css`
+    display: flex;
+    justify-content: center;
+    
+    width: 100%;
+    height: 20%;
+
+    border: 1px solid black;
+    
+    background-color: white;
+    
+    cursor: pointer;
+`;
+
+export const searchSortButton = css`
+    width: 80%;
+    height: 100%;
+
+    border: none;
+    font-size: 15px;
+    background-color: white;
+`;
+export const direction = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 20%;
+    height: 100%;
+
+    font-size: 20px;
+    border: none;
+`;
+
+export const sortCategoryList = css`
+    width: 100%;
+    
+    border: 1px solid black;
+    border-top: none;
+
+    background-color: white;
+
+    cursor: pointer;
+`;
+
+export const sortCategory = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    margin-top: 15px;
+    margin-bottom: 10px;
+
+    background-color: white;
+`;
+
+export const searchResultPanel = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    padding: 20px 0px;
+    width: 100%;
+    height: 130px;
+
+    border-bottom: 1px solid #dbdbdb;
+
+    cursor: pointer;
+`;
+
+export const panelImgContainer = css`
+    width: 15%;
+`;
+
+export const panelImg = css`
+    width: 100%;
+`
+
+export const panelInfo = css`
+    display: flex;
+    flex-direction: column;
+
+    padding-left: 20px;
+    width: 65%;
+`;
+
+export const panelTitle = css`
+    display: flex;
+`;
+
+export const progress = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    padding: 5px;
+    width: 50px;
+    background-color: green;
+    color: white;
+
+    font-size: 12px;
+`;
+
+export const panelTitleText = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    padding: 5px;
+    font-size: 15px;
+`;
+
+export const centerName = css`
+    display: flex;
+    
+    padding-top: 10px;
+    font-size: 14px;
+`;
+
+export const panelTotalAmount = css`
+    display: flex;
+   
+    width:20%;
+
+    font-size: 20px;
+`;
+
+export const Amount = css`
+    margin: 0px 10px;
+    color: green;
+`;
