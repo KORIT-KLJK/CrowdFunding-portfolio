62fbd60777885bc451010b2d95a54ee07862f594
HeaganLee <ekzmfkdl12@gmail.com>
2023-05-08 11:31:40 +0900

    2023-05-08 결과창 카테고리 버튼 추가

--- Frontend/crowdfunding/src/pages/Search/Search.js
+++ Frontend/crowdfunding/src/pages/Search/Search.js
@@ -1,6 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import React from 'react';
+import React, { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 
 const searchMainContainer = css`
@@ -12,7 +12,7 @@ const searchMainContainer = css`
 
 const searchBox = css`
     padding: 0px;
-    margin: 20px auto;
+    margin: 50px auto;
     width: 760px;
     height: 50px;
     border: 2px solid green;
@@ -41,13 +41,106 @@ const searchButton = css`
     cursor: pointer;
 `
 
+const searchResultBox = css`
+    margin: auto;
+    margin-top: 100px;
+
+    display: flex;
+
+    width: 800px;
+`;
+
+const searchResultCategory = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+   
+    
+    width: 100%;
+    height: 50px;
+
+    border-bottom: 1px solid black;
+`;
+
+const falseCategoryButton = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    padding-bottom: 10px;
+    margin: 0px 50px;
+    width: 20%;
+    height: 100%;
+
+    border: none;
+    
+    background-color: white;
+    font-size: 30px;
+
+    cursor: pointer;
+`;
+
+const trueCategoryButton = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    padding-bottom: 10px;
+    margin: 0px 50px;
+    width: 20%;
+    height: 100%;
+
+    border: none;
+    
+    background-color: red;
+    font-size: 30px;
+    color: green;
+
+    cursor: pointer;
+`;
+
+const line = css`
+    background-color: green;
+`;
+const searchResult = css`
+
+`
+
 const Search = () => {
+    const [giveflage, setGiveflage] = useState(false);
+    const [fundflage, setFundflage] = useState(false);
+
+    const giveSateChange = () => {
+        if(!giveflage) {
+            setGiveflage(true)
+            setFundflage(false)
+        }
+    }
+
+    const fundSateChange = () => {
+        if(!fundflage) {
+            setFundflage(true)
+            setGiveflage(false)
+        }
+    }
+
     return (
         <div css={searchMainContainer}>
             <div css={searchBox}>
-                <input type="text" css={searchInput}/>
+                <input type="text" css={searchInput} placeholder='검색어를 입력해 주세요'/>
                 <button css={searchButton}><FiSearch/></button>
             </div>
+            <div css={searchResultBox}>
+                <div css={searchResultCategory}>
+                    <button onClick={giveSateChange} css={giveflage ? trueCategoryButton : falseCategoryButton}>기부</button>
+                    <button onClick={fundSateChange} css={fundflage ? trueCategoryButton : falseCategoryButton}>펀딩</button>
+                </div>
+                <div css={searchResult}>
+
+                </div>
+            </div>
+
         </div>
     );
 };
