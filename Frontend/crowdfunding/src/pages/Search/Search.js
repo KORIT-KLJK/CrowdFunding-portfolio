ac00a28dee5f93f5580f071fe49323c0cdcccaa9
HeaganLee <ekzmfkdl12@gmail.com>
2023-05-08 14:59:18 +0900

    2023-05-08  결과창 탑

--- Frontend/crowdfunding/src/pages/Search/Search.js
+++ Frontend/crowdfunding/src/pages/Search/Search.js
@@ -46,6 +46,8 @@ const searchResultBox = css`
     margin-top: 100px;
 
     display: flex;
+    flex-direction: column;
+    align-items: center;
 
     width: 800px;
 `;
@@ -54,8 +56,6 @@ const searchResultCategory = css`
     display: flex;
     justify-content: center;
     align-items: center;
-
-   
     
     width: 100%;
     height: 50px;
@@ -93,23 +93,123 @@ const trueCategoryButton = css`
 
     border: none;
     
-    background-color: red;
+    background-color: #003d1e;
     font-size: 30px;
-    color: green;
+    color: #00Ab33;
 
     cursor: pointer;
 `;
 
-const line = css`
-    background-color: green;
-`;
-const searchResult = css`
-
+const searchResultContainer = css`
+    margin-top: 30px;
+    width: 100%;
 `
+
+const searchResultTop = css`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const searchResultLeft = css`
+    margin-top: 7px;
+
+`;
+
+const searchResultRight = css`
+    display: flex;
+    justify-content: space-between;
+    width: 250px;
+    height: 50px;
+
+`;
+
+const newestButton = css`
+    margin-top: 10px;
+    
+    width: 30%;
+    height: 20px;
+
+    border: none;
+    background-color: white;
+`;
+
+const amountButton = css`
+    margin-top: 10px;
+    margin-right: 25px;
+    width: 30%;
+    height: 20px;
+
+    border: none;
+    background-color: white;
+`;
+
+const searchSortContainer = css`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    width: 40%;
+    height: 200px;
+`;
+
+const searchSortButtonContainer = css`
+    display: flex;
+    justify-content: center;
+    
+    width: 100%;
+    height: 20%;
+
+    border: 1px solid black;
+    
+    background-color: white;
+    
+    cursor: pointer;
+`;
+
+const searchSortButton = css`
+    width: 80%;
+    height: 100%;
+
+    border: none;
+    font-size: 15px;
+    background-color: white;
+`
+const direction = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 20%;
+    height: 100%;
+
+    font-size: 20px;
+    border: none;
+`
+
+const sortCategoryList = css`
+    width: 100%;
+    
+    border: 1px solid black;
+    border-top: none;
+    cursor: pointer;
+`
+
+const sortCategory = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    margin-top: 15px;
+    margin-bottom: 10px;
+`
+
 
 const Search = () => {
     const [giveflage, setGiveflage] = useState(false);
     const [fundflage, setFundflage] = useState(false);
+
+    const [sortHidenFlage, setSortHidenFlage] = useState(false);
+
+    const [sortWord, setSortWord] = useState("전체");
 
     const giveSateChange = () => {
         if(!giveflage) {
@@ -125,6 +225,18 @@ const Search = () => {
         }
     }
 
+    const sortHiden = () => {
+        if(sortHidenFlage) {
+            setSortHidenFlage(false)
+        } else {
+            setSortHidenFlage(true)
+        }
+    }
+
+    const sortWordChange = (e) => {
+        setSortWord(e.target.textContent);
+    }
+
     return (
         <div css={searchMainContainer}>
             <div css={searchBox}>
@@ -136,8 +248,26 @@ const Search = () => {
                     <button onClick={giveSateChange} css={giveflage ? trueCategoryButton : falseCategoryButton}>기부</button>
                     <button onClick={fundSateChange} css={fundflage ? trueCategoryButton : falseCategoryButton}>펀딩</button>
                 </div>
-                <div css={searchResult}>
-
+                <div css={searchResultContainer}>
+                    <div css={searchResultTop}>
+                        <div css={searchResultLeft}>검색결과 " " 건</div>
+                        <div css={searchResultRight}>
+                           <button css={newestButton}>최신순</button>
+                           <button css={amountButton}>금액순</button>
+                           <div css={searchSortContainer}>
+                                <div css={searchSortButtonContainer} onClick={sortHiden}>
+                                    <button css={searchSortButton} >{sortWord}</button>
+                                    <div css={direction}>{sortHidenFlage ? "∧" : "∨"} </div>
+                                </div>
+                                {sortHidenFlage ? (<ul css={sortCategoryList}>
+                                    <li css={sortCategory} onClick={sortWordChange}>전체</li>
+                                    <li css={sortCategory} onClick={sortWordChange}>진행중</li>
+                                    <li css={sortCategory} onClick={sortWordChange}>종료</li>
+                                    </ul>) : ""}
+                                
+                           </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
