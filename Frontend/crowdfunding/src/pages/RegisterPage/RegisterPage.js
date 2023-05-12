20cbb9ed6284935d8195f4fa5fc9137bcf3a73bf
HeaganLee <ekzmfkdl12@gmail.com>
2023-05-12 12:09:11 +0900

    2023-05-12 등록페이지 파일 만듬

---
+++ Frontend/crowdfunding/src/pages/RegisterPage/RegisterPage.js
@@ -0,0 +1,14 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import React from 'react';
+import * as S from './style'
+
+const RegisterPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default RegisterPage;
