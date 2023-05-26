fa6e65798efad821615f4609a0a3c26f97331a5e
dpeps456789 <dpeps456789@naver.com>
2023-05-26 09:10:01 +0900

    로그인창 디자인 작업중

--- Frontend/crowdfunding/src/pages/Login/Login.js
+++ Frontend/crowdfunding/src/pages/Login/Login.js
@@ -6,12 +6,40 @@ import { useRecoilState } from 'recoil';
 import { authenticatedState } from './AuthAtom';
 import { useMutation } from 'react-query';
 
-export const mainContainer = css`
-    width: 700px;
+const mainContainer = css`
+    width: 1334px;
+    height: 529px;
     margin: auto;
     margin-top: 100px;
 `;
 
+const loginContainer = css`
+    box-sizing: border-box;
+    width: 460px;
+    margin: 0 auto;
+`;
+
+const loginUl = css`
+    position: relative;
+    z-index: 3;
+    margin-top: -8px;
+`;
+
+const loginLi = css`
+    border: 1px solid #c6c6c6;
+    border-radius: 6px;
+    background-color: #fff;
+    box-shadow: 0 5px 8px 0 rgba(68,68,68,.04);
+`;
+
+const loginInner = css`
+    padding: 20px 28px;
+`;
+
+
+const loginIdPwBox = css`
+    display: block;
+`
 
 const Login = () => {
     const [loginUser, setLoginUser] = useState({email: "", password: ""});
@@ -47,11 +75,21 @@ const Login = () => {
 
     return (
         <div css={mainContainer}>
-            <input placeholder="Email" type="text" onChange={informationHandle} name="email" />
-            <div>{errorMessages.email}</div>
-            <input placeholder="Password" type="password" onChange={informationHandle} name="password" />
-            <div>{errorMessages.password}</div>
-            <button onClick={loginHandleSubmit}>Sign in</button>
+            <div css={loginContainer}>
+                <ul css={loginUl}>
+                    <li css={loginLi}>
+                        <div css={loginInner}>
+                            <div css={loginIdPwBox}>
+                                <input placeholder="Email" type="text" onChange={informationHandle} name="email" />
+                                <div>{errorMessages.email}</div>
+                                <input placeholder="Password" type="password" onChange={informationHandle} name="password" />
+                                <div>{errorMessages.password}</div>
+                                <button onClick={loginHandleSubmit}>Sign in</button>
+                            </div>
+                        </div>                       
+                    </li>
+                </ul>
+            </div>         
         </div>
     );
 };
